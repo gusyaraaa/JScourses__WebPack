459ef30b1e278e55292008983aad47c3989a7035
Dmitry Bekker <gusyaraaa@yandex.ru>
2021-07-16 23:23:01 +0300

    deleted jquery

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,3 +1,11 @@
-import $ from "jquery";
+// import $ from "jquery";
 
-$("<h1 />").text("Hello world from JQuery").appendTo($("header"));
+// $("<h1 />").text("Hello world from JQuery").appendTo($("header"));
+
+const header = document.querySelector("header");
+
+header.insertAdjacentHTML("afterbegin", "<h1></h1>");
+
+const h1 = document.querySelector("h1");
+
+h1.textContent = "Hello world from JQuery";
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require("path");
 const HTMLPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "dist/",
   },
   plugins: [
     new HTMLPlugin({
